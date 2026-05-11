21b029822a9722179c75277c31feeb7c75f280cf
HP <jairam9003@gmail.com>
2026-05-11 10:37:21 +0530

    Add - Login and register

---
+++ turf-booking-frontend/src/app/core/services/auth-service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  private apiUrl =
+    'https://localhost:5001/api/auth';
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(data: any) {
+
+    return this.http.post(
+      `${this.apiUrl}/login`,
+      data
+    );
+
+  }
+
+}
