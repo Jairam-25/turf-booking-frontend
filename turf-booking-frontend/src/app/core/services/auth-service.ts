961d20fdbf51ce2f00426d45773a0c77b7fe26dc
HP <jairam9003@gmail.com>
2026-05-19 11:26:56 +0530

    API versoning

--- turf-booking-frontend/src/app/core/services/auth-service.ts
+++ turf-booking-frontend/src/app/core/services/auth-service.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   private apiUrl =
-    'https://localhost:5001/api/auth';
+    'https://localhost:5001/api/v1/auth';
 
   constructor(private http: HttpClient) {
   }
